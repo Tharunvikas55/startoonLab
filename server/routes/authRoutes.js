bcaa9382345381870f9490de8c4c6bd066eca676
Tharun Vikas SS <115332693+Tharunvikas55@users.noreply.github.com>
2024-08-22 06:13:54 +0530

    Update authRoutes.js

--- server/routes/authRoutes.js
+++ server/routes/authRoutes.js
@@ -7,6 +7,6 @@ const router = express.Router();
 router.post('/login', login);
 router.post('/register', register);
 router.post('/logout', logout);
-router.get('/',(req,res)=>{})
+router.get('/',(req, res,next) => res.json({message:"Hello world"}))
 
 module.exports = router;
