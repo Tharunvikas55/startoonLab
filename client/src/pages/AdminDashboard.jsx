85fa5ac083ab82b6770c830da687dfde42b17480
Tharun Vikas SS <115332693+Tharunvikas55@users.noreply.github.com>
2024-08-22 15:00:43 +0530

    Update AdminDashboard.jsx

--- client/src/pages/AdminDashboard.jsx
+++ client/src/pages/AdminDashboard.jsx
@@ -4,7 +4,8 @@ import { useNavigate } from 'react-router-dom';
 import { useSearchParams } from 'react-router-dom';
 import NavBar from '../components/NavBar';
 
-const baseURL = process.env.REACT_APP_API_URL;
+//const baseURL = process.env.REACT_APP_API_URL;
+const baseURL = 'https://startoonlab-server.onrender.com/api';
 
 const AdminDashboard = () => {
   const [users, setUsers] = useState([]);
