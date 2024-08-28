4e2388df435451e6f66eedfa96ef3df77070a8bf
Tharun Vikas SS <115332693+Tharunvikas55@users.noreply.github.com>
2024-08-28 20:31:39 +0530

    Update DashBoard.jsx

--- client/src/pages/DashBoard.jsx
+++ client/src/pages/DashBoard.jsx
@@ -2,7 +2,7 @@ import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 import { useNavigate } from 'react-router-dom';
 //const baseURL = process.env.REACT_APP_API_URL;
-const baseURL = 'https://startoonlab-server.onrender.com/api';
+const baseURL = 'https://startoonlab-server.onrender.com';
 
 const Profile = () => {
   const [user, setUser] = useState(null);
