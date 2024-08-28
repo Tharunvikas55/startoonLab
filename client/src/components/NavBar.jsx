8649b18cebdfa9b79063fcc9c8f22bee489a9ca4
Tharun Vikas SS <115332693+Tharunvikas55@users.noreply.github.com>
2024-08-28 21:27:48 +0530

    Update NavBar.jsx

--- client/src/components/NavBar.jsx
+++ client/src/components/NavBar.jsx
@@ -3,8 +3,8 @@ import { Link, useNavigate } from 'react-router-dom';
 import axios from 'axios';
 import Search from "./Search";
 
-//const baseURL = process.env.REACT_APP_API_URL;
-const baseURL = 'https://startoonlab-server.onrender.com';
+const baseURL = process.env.REACT_APP_API_URL;
+//const baseURL = 'https://startoonlab-server.onrender.com';
 const NavBar = ({ formatDataForChart, totalUser,totalClickCount }) => {
   const navigate = useNavigate();
 
